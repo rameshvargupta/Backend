433628e3965786302ce2db1cf593c98713b425a8
rameshvargupta <rameshvargupta230@gmail.com>
2026-03-29 14:11:36 +0530

    some cahnges

--- server.js
+++ server.js
@@ -18,13 +18,16 @@ const app = express();
 
 
 app.use(cors({
-  origin: "*",
+  origin: [
+    "http://localhost:5173",
+    "https://gtshop.online/"
+  ],
   credentials: true
 }));
 app.use(express.json());
 
 /* DB connect (IMPORTANT) */
-await connectDB();
+connectDB();
 
 /* TEST */
 app.get("/", (req, res) => {
